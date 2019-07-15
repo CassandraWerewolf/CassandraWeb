f4f810470aed25d0c5dbe24f95b9549cd71d361c
Caitlin Woodward <caitlin@simplefocus.com>
2019-07-15 07:51:44 +0000

    Simplify handling of funding message

--- werewolf/index.php
+++ werewolf/index.php
@@ -31,6 +31,27 @@ echo "<html> <head> <title>BGG Werewolf Stats</title>";
 <?php display_menu() ?>
 <center>
 
+<?php if ($show_funding_message) { ?>
+	<div style="padding: 0 20%;">
+		<h1>2019 Fundraiser</h1>
+		To provide high performance and high availablity and continuously backed up web site Cassandra is hosted on Amazon AWS. This cost money and we hope that our community will help contibute to that cost.<br>
+		<h3 style="margin: 5px 0">Goal: $60/month</h3><br>
+		<h5 style="margin: 5px 0">$2/month from 30 or more people</h3><br>
+		<div style="float: left; padding: 0 0 0 28%;">
+		<a href="https://www.paypal.me/CassandraWerewolf/24">
+		<img alt="donate button" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0">
+		</div>
+		<div style="float: left; margin: 5px;">
+		<h3>OR</h3>
+		</div>	
+		<div style="float: left">
+		<a href="https://www.patreon.com/bePatron?u=10619201" data-patreon-widget-type="become-patron-button">Become a Patron!</a><script async src="https://c6.patreon.com/becomePatronButton.bundle.js"></script>
+		</a>
+		</div>
+		<div style="clear: both;"></div>
+	</div>
+<?php } ?>
+
 <h1>BGG Werewolf Stats</h1>
 
 <?php
--- werewolf/setup.php
+++ werewolf/setup.php
@@ -5,4 +5,7 @@ define(ROOT_PATH, dirname(__FILE__), true);
 // CSS filename should match the theme name here, ex: 'matrix' with matrix.css
 $theme = 'default';
 
+// Periodically we will ask for donations in a fundraiser.
+// Setting this to true will show a message on the main Cassy index.
+$show_funding_message = false;
 ?>
