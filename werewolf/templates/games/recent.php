e8241c995165aa0ca60d7bd8627cab615226455b
Caitlin Woodward <caitlin@simplefocus.com>
2019-09-08 03:54:35 +0000

    Refactor the homepage

---
+++ werewolf/templates/games/recent.php
@@ -0,0 +1,231 @@
+<div class='page-title'>
+    <h1>BGG Werewolf Stats</h1>
+</div>
+
+<table class='games-counts forum_table' border='0'>
+    <tr>
+        <th>Game #'s</th>
+        <th>Total</th>
+        <th>Won by Evil</th>
+        <th>Won by Good</th>
+        <th>Other type of game</th>
+        <th>In Progress</th>
+    </tr>
+    <tr>
+        <td></td>
+        <?php foreach ( $games->get_stats() as $type => $count ) { ?>
+            <td><a href='show_games.php?type=<?php echo $type ?>'><?php echo $count ?></a></td>
+        <?php } ?>
+    </tr>
+</table>
+
+
+<div class='games-index'>
+    <div class='games-index--progress'>
+        
+        <?php if ( count($games_in_fast_progress) > 0 ) {?>
+            <table width='100%' class='games-index--section forum_table' cellpadding='2'>
+                <tr>
+                    <th colspan=2>In Progress (Fast)</th>
+                    <th>Day</th>
+                    <th>Night</th>
+                </tr>
+                <?php foreach ( $games_in_fast_progress as $game ) { ?>
+                    <tr>
+                        <td><?php echo $game['number'] . ")" ?></td>
+                        <td>
+                            <?php if ($game['notifications']['needs_replacement']) { ?>
+                                <img src='/images/i_replace.png' border='0'/>
+                            <?php } ?>
+                            <?php if ($game['notifications']['signed_up']) { ?>
+                                <img src='/images/calendar.png' />
+                            <?php } ?>
+                            <?php if ($game['notifications']['new_chat']) { ?>
+                                <a href='/game/<?php echo $game['thread_id'] ?>/chat'><img src='/images/new_message.png' border='0'/></a>
+                            <?php } ?>
+                            <a href='/game/<?php echo $game['thread_id'] ?>'><?php echo $game['title'] ?></a>
+                        </td>
+                        <td><?php echo $game['day_length'] ?></td>
+                        <td><?php echo $game['night_length'] ?></td>
+                    </tr>
+                <?php } ?>
+            </table>
+        <?php } ?>
+
+        <table width='100%' class='games-index--section forum_table' cellpadding='2'>
+            <tr>
+                <th colspan=2>In Progress (Standard)</th>
+                <th>Day</th>
+                <th>Night</th>
+            </tr>
+            <?php foreach ( $games_in_standard_progress as $game ) { ?>
+                <tr>
+                    <td><?php echo $game['number'] . ")" ?></td>
+                    <td>
+                        <?php if ($game['notifications']['needs_replacement']) { ?>
+                            <img src='/images/i_replace.png' border='0'/>
+                        <?php } ?>
+                        <?php if ($game['notifications']['signed_up']) { ?>
+                            <img src='/images/calendar.png' />
+                        <?php } ?>
+                        <?php if ($game['notifications']['new_chat']) { ?>
+                            <a href='/game/<?php echo $game['thread_id'] ?>/chat'><img src='/images/new_message.png' border='0'/></a>
+                        <?php } ?>
+                        <a href='/game/<?php echo $game['thread_id'] ?>'><?php echo $game['title'] ?></a>
+                    </td>
+                    <td><?php echo $game['lynch_time'] ?></td>
+                    <td><?php echo $game['na_deadline'] ?></td>
+                </tr>
+            <?php } ?>
+        </table>
+
+        <table width='100%' class='forum_table' cellpadding='2'>
+            <tr><th colspan=2>10 Most Recently Ended</th></tr>
+            <?php foreach ( $games_recently_ended as $game ) { ?>
+                <tr>
+                    <td><?php echo $game['number'] . ")" ?></td>
+                    <td>
+                        <?php if ($game['notifications']['signed_up']) { ?>
+                            <img src='/images/calendar.png' />
+                        <?php } ?>
+                        <?php if ($game['notifications']['new_chat']) { ?>
+                            <a href='/game/<?php echo $game['thread_id'] ?>/chat'><img src='/images/new_message.png' border='0'/></a>
+                        <?php } ?>
+                        <a href='/game/<?php echo $game['thread_id'] ?>'><?php echo $game['title'] ?></a>
+                    </td>
+                </tr>
+            <?php } ?>
+        </table>
+
+    </div>
+
+    <div class='games-index--signup'>
+
+        <table width='100%' class='games-index--section forum_table' cellpadding='2'>
+            <tr>
+                <th>In Signup (Fast)</th>
+                <th>Moderator</th>
+                <th>Start Date</th>
+                <th>Dusk</th>
+                <th>Dawn</th>
+            </tr>
+            <?php foreach ( $games_in_fast_signup as $game ) { ?>
+                <tr>
+                    <td>
+                        <img src='/images/<?php echo $game['complex'] ?>_small.png' alt='<?php echo $game['complex'] ?>' />
+                        <?php if ($game['notifications']['signed_up']) { ?>
+                            <img src='/images/calendar.png' />
+                        <?php } ?>
+                        <?php if ($game['notifications']['new_chat']) { ?>
+                            <a href='/game/<?php echo $game['thread_id'] ?>/chat'><img src='/images/new_message.png' border='0'/></a>
+                        <?php } ?>
+                        <a href='/game/<?php echo $game['thread_id'] ?>'><?php echo $game['title'] ?></a>
+                        <span>
+                            (<?php echo ($game['num_players'] == $game['max_players'] ? "Full" : $game['num_players']) ?>/<?php echo $game['max_players'] ?>)
+                        </span>
+                    </td>
+                    <td><?php echo ($game['swf'] == 'Yes' ? "When Full" : $game['start']) ?></td>
+                    <td>
+                        <?php 
+                            echo implode(
+                                array_map(
+                                    function($mod) { return "<a href='/player/$mod'>$mod</a>"; },
+                                    array_values($game['mods']
+                                )
+                            ), ', ');
+                        ?>
+                    </td>
+                    <td><?php echo $game['day_length'] ?></td>
+                    <td><?php echo $game['night_length'] ?></td>
+                </tr>
+            <?php } ?>
+        </table>
+
+        <table width='100%' class='games-index--section forum_table' cellpadding='2'>
+            <tr>
+                <th>In Signup (Standard)</th>
+                <th>Moderator</th>
+                <th>Start Date</th>
+                <th>Dusk</th>
+                <th>Dawn</th>
+            </tr>
+            <?php foreach ( $games_in_standard_signup_as_date as $game ) { ?>
+                <tr>
+                    <td>
+                        <img src='/images/<?php echo $game['complex'] ?>_small.png' alt='<?php echo $game['complex'] ?>' />
+                        <?php if ($game['notifications']['signed_up']) { ?>
+                            <img src='/images/calendar.png' />
+                        <?php } ?>
+                        <?php if ($game['notifications']['new_chat']) { ?>
+                            <a href='/game/<?php echo $game['thread_id'] ?>/chat'><img src='/images/new_message.png' border='0'/></a>
+                        <?php } ?>
+                        <a href='/game/<?php echo $game['thread_id'] ?>'><?php echo $game['title'] ?></a>
+                        <span>
+                            (<?php echo ($game['num_players'] == $game['max_players'] ? "Full" : $game['num_players']) ?>/<?php echo $game['max_players'] ?>)
+                        </span>
+                    </td>
+                    <td><?php echo $game['start'] ?></td>
+                    <td>
+                        <?php 
+                            echo implode(
+                                array_map(
+                                    function($mod) { return "<a href='/player/$mod'>$mod</a>"; },
+                                    array_values($game['mods']
+                                )
+                            ), ', ');
+                        ?>
+                    </td>
+                    <td><?php echo $game['lynch_time'] ?></td>
+                    <td><?php echo $game['na_deadline'] ?></td>
+                </tr>
+            <?php } ?>
+        </table>
+
+        <table width='100%' class='games-index--section forum_table' cellpadding='2'>
+            <tr>
+                <th>In Signup (Standard - Starts When Full)</th>
+                <th>Moderator</th>
+                <th>Needs</th>
+                <th>Dusk</th>
+                <th>Dawn</th>
+            </tr>
+            <?php foreach ( $games_in_standard_signup_as_swf as $game ) { ?>
+                <tr>
+                    <td>
+                        <img src='/images/<?php echo $game['complex'] ?>_small.png' alt='<?php echo $game['complex'] ?>' />
+                        <?php if ($game['notifications']['signed_up']) { ?>
+                            <img src='/images/calendar.png' />
+                        <?php } ?>
+                        <?php if ($game['notifications']['new_chat']) { ?>
+                            <a href='/game/<?php echo $game['thread_id'] ?>/chat'><img src='/images/new_message.png' border='0'/></a>
+                        <?php } ?>
+                        <a href='/game/<?php echo $game['thread_id'] ?>'><?php echo $game['title'] ?></a>
+                        <span>
+                            (<?php echo ($game['num_players'] == $game['max_players'] ? "Full" : $game['num_players']) ?>/<?php echo $game['max_players'] ?>)
+                        </span>
+                    </td>
+                    <td>
+                        <?php 
+                            echo implode(
+                                array_map(
+                                    function($mod) { return "<a href='/player/$mod'>$mod</a>"; },
+                                    array_values($game['mods']
+                                )
+                            ), ', ');
+                        ?>
+                    </td>
+                    <td><?php echo $game['players_needed'] ?></td>
+                    <td><?php echo $game['lynch_time'] ?></td>
+                    <td><?php echo $game['na_deadline'] ?></td>
+                </tr>
+            <?php } ?>
+        </table>
+
+        <span><b>Complexity Ratings: <img src='images/Newbie_large.png'><img src='images/Low_large.png'><img src='images/Medium_large.png'><img src='images/High_large.png'><img src='images/Extreme_large.png'></b></span>
+        <br />
+        <a href='create_a_game.php'>Add a Game in Signup</a>
+        <br />
+        <a href='automod/new.php'>Add an Auto-Mod Game</a>
+        
+    </div>
+</div>
---
+++ werewolf/templates/shared/footer.php
@@ -0,0 +1,47 @@
+  </div> <!-- end .content -->
+   
+   <!-- <img src='games_started_graph.php'> -->
+    <footer>
+      <div class="cassy-links">
+        <div class="cassy-links-section">
+          <h3>Cassandra</h3>
+          <ul>
+            <li><a href='signup.php'>Get a Password</a></li>
+            <li><a href='password.php'>Change Password</a></li>
+            <li><a href='show_active.php'>Active players and moderators</a></li>
+            <li><a href='show_cassandra_files.php'>Current games</a></li>
+            <li><a href='show_games_missing_info.php'>Games with missing data</a></li>
+            <li><a href='/automod/'>Create your own Automod Template</a></li>
+            <li><a href='/stats/'>Fun Statistics</a></li>
+            <li><a href='change_log.html' title='Last Updated: <?php echo date("l, d-M-Y", filemtime('change_log.html'));?>'>Change Log</a></li>
+          </ul>
+        </div>
+        <div class="cassy-links-section">
+          <h3>Community</h3>
+          <ul>
+            <li><a href='secrecy_pledge.html'>Our Pledge</a> - Please Read</li>
+            <li><a href='wotw.php'>Wolf of the Week List</a></li>
+            <li><a href='wolfy_awards.php'>Wolfy Awards</a></li>
+            <li><a href='ranks.php'>Player and moderator Ranks</a></li>
+            <li><a href='http://boardgamegeek.com/thread/225928'>Player Picture Thread</a> (<a href='game/225928'>By Player</a>)</li>
+            <li><a href='timezones.php'>Player Timezone Chart</a></li>
+            <li><a href='/social/'>Find WW players Elsewhere</a></li>
+            <?php if ( isset($username) ) { ?><li><a href='https://discord.gg/ftUvN3k' target="_blank">Discord Chat</a></li><?php } ?>
+          </ul>
+        </div>
+        <div class="cassy-links-section">
+          <h3>Tools</h3>
+          <ul>
+            <!-- <li><a href='/tools/aes/'>AES Encryption App</a></li> -->
+            <li><a href='/tools/rsa/'>RSA Encryption App</a></li>
+            <li><a href='/tools/shamir/'>Shamir Secret Sharing App</a></li>
+            <li><a href='/tools/bookmarklets/'>Bookmarklets</a></li>
+            <li><a href='/balance/'>Game Balance Creator</a></li>
+            <li><a href='/admin/'>Admin Pages</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="cassy-load-time"><?php $timer->end_time(); echo number_format($timer->elapsed_time(), 3) . " seconds"; ?></div>
+    </footer>
+  </body>
+</html>
