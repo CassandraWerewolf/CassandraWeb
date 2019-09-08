03eacf8ada4f565183d10eaf62396be72cf92e38
Caitlin Woodward <caitlin@simplefocus.com>
2019-09-08 03:54:35 +0000

    Refactor the homepage

---
+++ werewolf/templates/shared/header.php
@@ -0,0 +1,9 @@
+<html> 
+  <head>
+    <title>BGG Werewolf Stats</title>
+    <link rel='stylesheet' type='text/css' href='/assets/css/application.css'>
+    <?php if ($theme != 'default') { echo "<link rel='stylesheet' type='text/css' href='/assets/css/themes/{$theme}.css'>"; } ?>
+  </head>
+  <body>
+    <?php display_menu() ?>
+    <div class='content'>
