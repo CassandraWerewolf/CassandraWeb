26d656349af145df07dd7bc9dc65a079bf22ce3a
Caitlin Woodward <caitlin@simplefocus.com>
2019-11-06 03:30:55 +0000

    Rework header to responsive

--- werewolf/templates/shared/header.php
+++ werewolf/templates/shared/header.php
@@ -1,10 +1,11 @@
 <html> 
   <head>
     <title>BGG Werewolf Stats</title>
-    <link rel='stylesheet' type='text/css' href='/assets/css/application.css'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="/assets/css/application.css">
     <?php if ($theme != 'default') { echo "<link rel='stylesheet' type='text/css' href='/assets/css/themes/{$theme}.css'>"; } ?>
   </head>
   <body class="body">
     <?php display_menu() ?>
-    <div class='content-wrap'>
-      <div class='content'>
+    <div class="content-wrap">
+      <div class="content">
