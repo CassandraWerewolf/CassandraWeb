0e5121761d6d6fe16f4eb82445ada20c8f135a07
Caitlin Woodward <caitlin@simplefocus.com>
2021-10-30 09:30:12 +0000

    Fix authenticate script

--- bin/bgg/authenticate.pl
+++ bin/bgg/authenticate.pl
@@ -6,7 +6,7 @@ use LWP::UserAgent;
 use HTTP::Cookies;
 
 my $usage="\nUsage $0 username password\n\n";
-my $url='https://boardgamegeek.com/login';
+my $url='https://boardgamegeek.com/login/api/v1';
 
 # parse arguments
 die $usage unless ($#ARGV >= 1);
@@ -14,15 +14,27 @@ my $username = shift;
 my $password = shift;
 
 # CREATE USER AGENT 
-my $agent = LWP::UserAgent->new(cookie_jar => {});
+my $agent = LWP::UserAgent->new(
+    agent => 'CassandraWerewolf/1.0 https://cassandrawerewolf.com',
+    cookie_jar => {}
+);
 
-# compose the data
-my %message;
-$message{username}=$username;
-$message{password}=$password;
+# compose the request
+my $json = "{ 
+    \"credentials\": {
+        \"username\": \"$username\",
+        \"password\": \"$password\"
+    }
+}";
 
-# send data
-$agent->post($url, \%message, Referer => 'https://boardgamegeek.com/login');
+# send request
+my $request = HTTP::Request->new('POST', $url);
+$request->header( 
+    'Content-Type' => 'application/json',
+    'Referer' => 'https://boardgamegeek.com/'
+);
+$request->content($json);
+$agent->request($request);
 
 # retrieve sessionId from cookie jar
 print ($agent->cookie_jar->as_string =~ /SessionID=(\w+)/);
