33e4af3ea1e21179dd873c735432d9c4799df14c
Bob Archer <git@anon.rkamail.com>
2022-01-25 22:50:59 -0500

    Add authorization header to send geekmail

--- bin/bgg/send_geekmail.pl
+++ bin/bgg/send_geekmail.pl
@@ -3,16 +3,14 @@
 use strict;
 use LWP::UserAgent;
 
-my $agent;
 my $from;
 my $passwd;
 my $to;
 my $subject;
 my $body;
-my %login;
 my %message;
 
-my $login_url='https://boardgamegeek.com/login';
+my $login_url='https://boardgamegeek.com/login/api/v1';
 my $mail_url='https://boardgamegeek.com/geekmail_controller.php';
 
 die "\nUsage $0 from passwd to \"subject\" \"body\"\n\n" unless ($#ARGV == 4);
@@ -27,17 +25,15 @@ my $agent = LWP::UserAgent->new(
     cookie_jar => {}
 );
 
-$login{username}=$from;
-$login{password}=$passwd;
-$login{B1}='Submit';
-
 $message{action}='save';
 $message{B1}='Send';
 $message{touser}=$to;
 $message{subject}=$subject;
 $message{body}=$body;
 
-$agent->post($login_url, \%login);
-$agent->post($mail_url, \%message, Referer => 'https://boardgamegeek.com/geekmail');
+# authenticate
+my $geekauth = `/opt/werewolf/bgg/authenticate.pl "$from" $passwd`;
+
+$agent->post($mail_url, \%message, Referer => 'https://boardgamegeek.com/geekmail', Authorization => "GeekAuth $geekauth");
 
 exit;
